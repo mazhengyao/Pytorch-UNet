7606d1565887b06e134b6264a83ba75f18c2c0c5
mazhengyao <53358800+mazhengyao@users.noreply.github.com>
2022-09-29 14:38:52 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ from unet import UNet
 
 dir_img = Path('./data/imgs/')
 dir_mask = Path('./data/masks/')
-dir_checkpoint = Path('./checkpoints/')
+dir_checkpoint = Path('./')
 
 
 def train_net(net,
