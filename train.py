049dba689a09155f4be2fe2d5c5aa1c408b810b2
mazhengyao <53358800+mazhengyao@users.noreply.github.com>
2022-09-29 12:47:47 +0800

    Update train.py

--- train.py
+++ train.py
@@ -111,7 +111,7 @@ def train_net(net,
                 pbar.set_postfix(**{'loss (batch)': loss.item()})
 
                 # Evaluation round
-                division_step = (n_train // (10 * batch_size))
+                division_step = (n_train // (100 * batch_size))
                 if division_step > 0:
                     if global_step % division_step == 0:
                         histograms = {}
